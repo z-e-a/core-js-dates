3fcae347c951f8c5e611f9ae5351fe6c39019f54
z-e-a <e.a.zubkov@yandex.ru>
2024-03-22 13:55:16 +0300

    feat: solve task 'getCountDaysOnPeriod'

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -101,8 +101,10 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  return (
+    1 + (Date.parse(dateEnd) - Date.parse(dateStart)) / (24 * 60 * 60 * 1000)
+  );
 }
 
 /**
