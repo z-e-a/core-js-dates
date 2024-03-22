b7c5673495e825f8796b391d609ab07e1108239d
z-e-a <e.a.zubkov@yandex.ru>
2024-03-22 11:29:08 +0300

    feat: solve task 'getDayName'

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -46,8 +46,16 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  return [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wensday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ][new Date(Date.parse(date)).getDay()];
 }
 
 /**
