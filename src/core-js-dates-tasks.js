323548d1f25c378dd3f3d68bafc9bce8c45916df
z-e-a <e.a.zubkov@yandex.ru>
2024-03-22 14:04:27 +0300

    feat: solve task 'isDateInPeriod'

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -124,8 +124,11 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  return (
+    Date.parse(period.start) <= Date.parse(date) &&
+    Date.parse(date) <= Date.parse(period.end)
+  );
 }
 
 /**
